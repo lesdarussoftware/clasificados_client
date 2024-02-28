727246ecd1afcbbe5cb14d1f075ee085046ff2fb
matias ramos <matiasramos261@gmail.com>
2024-02-28 20:00:39 -0300

    ui form

---
+++ src/components/Dialog.jsx
@@ -0,0 +1,7 @@
+export function Dialog({ children }) {
+    return (
+        <dialog id="dialog">
+            {children}
+        </dialog>
+    )
+}
--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,10 +1,26 @@
 import { useCategories } from "../hooks/useCategories";
 import { AdminLayout } from "../components/AdminLayout";
 import { Table } from "../components/Table";
+import { Dialog } from "../components/Dialog";
+import { useState } from "react";
 
 export function Categories() {
 
     const { categories } = useCategories()
+
+    const handleOpen = () => {
+        const dialog = document.querySelector('dialog')
+        dialog.showModal()
+    }
+
+    const handleClose = () => {
+        const dialog = document.querySelector('dialog')
+        dialog.close()
+    }
+
+    const handleSubmit = () => {
+
+    }
 
     const columns = [
         {
@@ -25,8 +41,24 @@ export function Categories() {
         <AdminLayout>
             <div className="adminPageHeader">
                 <h2>Categorías</h2>
-                <button>Nueva</button>
+                <button onClick={handleOpen}>Nueva</button>
             </div>
+            <Dialog open={open === 'NEW' || open === 'EDIT'}>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="name">Nombre</label>
+                        <input type="text" id="name" name="name" />
+                    </div>
+                    <div className="form-footer">
+                        <button type="button" className="cancel-button" onClick={handleClose}>
+                            Cancelar
+                        </button>
+                        <button type="button" onClick={handleSubmit}>
+                            Guardar
+                        </button>
+                    </div>
+                </form>
+            </Dialog>
             <Table
                 columns={columns}
                 rows={categories}
