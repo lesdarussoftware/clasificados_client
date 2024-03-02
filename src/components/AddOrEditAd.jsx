dccaa25282a0fd271a3c1ee318223d281ac893f5
matias ramos <matiasramos261@gmail.com>
2024-03-01 21:34:22 -0300

    form

--- src/components/AddOrEditAd.jsx
+++ src/components/AddOrEditAd.jsx
@@ -1,4 +1,7 @@
+import 'leaflet/dist/leaflet.css';
+
 import { Dialog } from "./Dialog";
+import {  Map } from './MapComponent';
 
 export function AddOrEditAd({
     action,
@@ -16,6 +19,11 @@ export function AddOrEditAd({
 
     const width = isPrivate ? '25%' : '33%'
 
+    var map = L.map('map', {
+        center: [51.505, -0.09],
+        zoom: 13
+    });
+
     return (
         <Dialog type="new-edit" top={0} left={0} width="100%">
             <h3>{action === 'NEW' ? 'Nuevo aviso' : `Editar el aviso #${formData.id}`}</h3>
@@ -24,22 +32,8 @@ export function AddOrEditAd({
                     <div style={{ display: 'flex', flexDirection: 'column', width }}>
                         <div className="form-group">
                             <label htmlFor="content">Contenido</label>
-                            <textarea name="content" id="content" value={formData.content} onChange={handleChange} ></textarea>
+                            <textarea name="content" id="content" value={formData.content} onChange={handleChange} rows={17} ></textarea>
                             {errors.content?.type === 'maxLength' && <small>* El contenido es demasiado largo.</small>}
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="category_id">Categoría</label>
-                            <select name="category_id" id="category_id" onChange={handleChange} value={formData.category_id}>
-                                <option value="">Seleccione</option>
-                                {categories.map(cat => (
-                                    <option key={cat.id} value={cat.id}>{cat.name}</option>
-                                ))}
-                            </select>
-                            {errors.category_id?.type === 'required' && <small>* La categoría es requerida.</small>}
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="duration">Duración (días)</label>
-                            <input type="number" min={1} max={7} step={1} name="duration" id="duration" value={formData.duration} onChange={handleChange} />
                         </div>
                     </div>
                     <div style={{ display: 'flex', flexDirection: 'column', width }}>
@@ -68,13 +62,13 @@ export function AddOrEditAd({
                             <input type="text" name="address" id="address" value={formData.address} onChange={handleChange} />
                             {errors.address?.type === 'maxLength' && <small>* La dirección es demasiado larga.</small>}
                         </div>
-                    </div>
-                    <div style={{ display: 'flex', flexDirection: 'column', width }}>
                         <div className="form-group">
                             <label htmlFor="phone">Teléfono</label>
                             <input type="text" name="phone" id="phone" value={formData.phone} onChange={handleChange} />
                             {errors.phone?.type === 'maxLength' && <small>* El teléfono es demasiado largo.</small>}
                         </div>
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'column', width }}>
                         <div className="form-group">
                             <label htmlFor="email">Email</label>
                             <input type="email" name="email" id="email" value={formData.email} onChange={handleChange} />
@@ -85,21 +79,37 @@ export function AddOrEditAd({
                             <input type="text" name="link" id="link" value={formData.link} onChange={handleChange} />
                             {errors.link?.type === 'maxLength' && <small>* El enlace es demasiado largo.</small>}
                         </div>
+                        <div className="form-group">
+                            <label htmlFor="category_id">Categoría</label>
+                            <select name="category_id" id="category_id" onChange={handleChange} value={formData.category_id}>
+                                <option value="">Seleccione</option>
+                                {categories.map(cat => (
+                                    <option key={cat.id} value={cat.id}>{cat.name}</option>
+                                ))}
+                            </select>
+                            {errors.category_id?.type === 'required' && <small>* La categoría es requerida.</small>}
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="duration">Duración (días)</label>
+                            <input type="number" min={1} max={7} step={1} name="duration" id="duration" value={formData.duration} onChange={handleChange} />
+                        </div>
                     </div>
                     {isPrivate &&
                         <div style={{ display: 'flex', flexDirection: 'column', width }}>
-                            <div className="form-group">
-                                <label htmlFor="file">Archivo</label>
-                                <input type="file" name="file" id="file" value={formData.file} onChange={handleChange} />
-                                {errors.file?.type === 'maxLength' && <small>* El nombre del archivo es demasiado largo.</small>}
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="is_visible">Visible</label>
-                                <input type="checkbox" name="is_visible" id="is_visible"
-                                    checked={formData.is_visible}
-                                    onChange={(e) => handleChange({ target: { name: 'is_visible', value: e.target.checked } })}
-                                />
-                            </div>
+                            <>
+                                <div className="form-group">
+                                    <label htmlFor="file">Archivo</label>
+                                    <input type="file" name="file" id="file" value={formData.file} onChange={handleChange} />
+                                    {errors.file?.type === 'maxLength' && <small>* El nombre del archivo es demasiado largo.</small>}
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="is_visible">Visible</label>
+                                    <input type="checkbox" name="is_visible" id="is_visible"
+                                        checked={formData.is_visible}
+                                        onChange={(e) => handleChange({ target: { name: 'is_visible', value: e.target.checked } })}
+                                    />
+                                </div>
+                            </>
                         </div>
                     }
                 </div>
@@ -112,6 +122,7 @@ export function AddOrEditAd({
                     </button>
                 </div>
             </form>
+                <Map />
         </Dialog>
     )
 }
--- src/hooks/useCategories.jsx
+++ src/hooks/useCategories.jsx
@@ -18,7 +18,11 @@ export function useCategories() {
     async function getCategories() {
         const { status, data } = await get()
         if (status === 200) {
-            setCategories(data)
+            setCategories(data.sort((a, b) => {
+                if (a.name > b.name) return 1
+                if (a.name < b.name) return -1
+                return 0
+            }))
             setLoadingCategories(false)
         }
     }
